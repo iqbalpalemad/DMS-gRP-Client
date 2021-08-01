c378cebcf10566b21612bf4c9a51b826cc84c425
iqbalpalemad <iqbal@nethram.com>
2021-08-01 12:22:08 +0530

    API to get file details

---
+++ Controller/file/get.js
@@ -0,0 +1,28 @@
+const grpcFileClient            = require('../../grpcClient/grpcFileClient')
+
+const getFile   = (req,res) => {
+    try{
+        grpcClient = grpcFileClient()
+        const fileRequest = {
+            token  : req.token,
+            name : "",
+            parentFolderId : "",
+            fileId : req.params.fileId
+        }
+
+        grpcClient.get(fileRequest, (err, response) => {
+            if(err){
+                return res.status(500).json({result : false, error : err.message});
+            }
+            if(!response.result){
+                return res.status(400).json(response);
+            }
+            return res.status(201).json(response);
+        })
+    }
+    catch(err){
+        return res.status(500).json({result : false, error : err.message});
+    }
+}
+
+module.exports = getFile;
--- Routes/file.js
+++ Routes/file.js
@@ -7,10 +7,12 @@ const createFile    = require('../Controller/file/create');
 const updateFile    = require('../Controller/file/update');
 const deleteFile    = require('../Controller/file/delete');
 const moveFile      = require('../Controller/file/move');
+const getFile       = require('../Controller/file/get');
 
 router.post('/',userAuth,validate.fileCreateValidation(),createFile);
 router.put('/:fileId',userAuth,validate.fileUpdateValidation(),updateFile);
 router.delete('/:fileId',userAuth,deleteFile);
 router.post('/move/:fileId/:parentFolderId',userAuth,moveFile)
+router.get('/:fileId',userAuth,getFile);
 module.exports = router;
 
