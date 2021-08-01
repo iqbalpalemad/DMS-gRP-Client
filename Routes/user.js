a06f9cffa8ff4217087c84de86985db284b61c70
iqbalpalemad <iqbal@nethram.com>
2021-08-01 16:09:22 +0530

    API to delete user

--- Routes/user.js
+++ Routes/user.js
@@ -3,8 +3,10 @@ const router            = express.Router();
 const userAuth          = require('../Middleware/userAuth');
 
 const getAllUsers       = require('../Controller/users/getAll');
+const deleteUser        = require('../Controller/users/delete');
 
 router.get('/',userAuth,getAllUsers);
+router.delete('/:userId',userAuth,deleteUser);
 
 
 module.exports = router;
